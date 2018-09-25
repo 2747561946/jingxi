55db55ad6f3d3c6034da18e456e06734cc7b807c
lf <54654@email.com>
2018-09-25 21:10:34 +0800

    代码自动生成器

---
+++ controllers/CodeController.php
@@ -0,0 +1,48 @@
+<?php
+namespace controllers;
+
+class CodeController
+{
+    // 生成代码
+    public function make()
+    {
+        $tableName = $_GET['name'];
+        // 控制器
+        $cname = ucfirst($tableName).'Controller';
+        // echo $cname;
+
+        // 加载模板
+        ob_start();
+        include(ROOT . 'templates/controller.php');
+        $str = ob_get_clean();
+        file_put_contents(ROOT.'controllers/'.$cname.'.php',"<?php\r\n".$str);
+
+        // 生成模型
+        $mname = ucfirst($tableName);
+        ob_start();
+        include(ROOT . 'templates/model.php');
+        $str = ob_get_clean();
+        file_put_contents(ROOT.'models/'.$mname.'.php',"<?php\r\n".$str);
+
+        // 生成视图
+        @mkdir(ROOT . 'views/'.$tableName,0777);
+
+        // create.html
+        ob_start();
+        include(ROOT . 'templates/create.html');
+        $str = ob_get_clean();
+        file_put_contents(ROOT.'views/'.$tableName.'/create.html',$str);
+
+        // edit.html
+        ob_start();
+        include(ROOT . 'templates/edit.html');
+        $str = ob_get_clean();
+        file_put_contents(ROOT.'views/'.$tableName.'/edit.html',$str);
+
+        // index.html
+        ob_start();
+        include(ROOT.'templates/index.html');
+        $str = ob_get_clean();
+        file_put_contents(ROOT.'views/'.$tableName.'/index.html',$str);
+    }
+}
